d57ff9beb345858d6db35ea9b0ec3370cb82dc96
rahulroyrdx001 <rahulroyrdx001@gmail.com>
2024-04-24 18:36:35 +0530

    Version 1.0.0.7

--- TMS/src/components/Modal.jsx
+++ TMS/src/components/Modal.jsx
@@ -1,24 +1,57 @@
+import { useState } from "react";
 import style from "../styling/Modal.module.css";
+import axios from "axios";
 
 function Modal() {
+  const [user, setUser] = useState({
+    headline: "",
+    description: "",
+  });
+
+  let name, value;
+  const handleInput = (e) => {
+    name = e.target.name;
+    value = e.target.value;
+    setUser({ ...user, [name]: value });
+    console.log(e.target.value);
+  };
+
+  const postData = async (e) => {
+    e.preventDefault();
+    console.log(user);
+    try {
+      axios.post("http://localhost:8000/", { user });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
     <>
       <div className={style.mybox}>
-        <form className={style.mybox1}>
+        <form method="POST" className={style.mybox1}>
           <h3>Headline</h3>
           <input
+            name="headline"
             type="text"
+            onChange={handleInput}
+            value={user.headline}
             className={style.headline}
             placeholder="Enter Headline"
           ></input>
           <h3>Description</h3>
           <textarea
-            type="text"
+            name="description"
+            typeof="text"
+            onChange={handleInput}
+            value={user.description}
             className={style.description}
             placeholder="Enter Description"
           ></textarea>
           <button className={style.discard}>Discard</button>
-          <button className={style.submit}>Submit</button>
+          <button type="submit" className={style.submit} onClick={postData}>
+            Submit
+          </button>
         </form>
       </div>
     </>
--- TMS/src/components/MyWorkingarea.jsx
+++ TMS/src/components/MyWorkingarea.jsx
@@ -9,7 +9,7 @@ function MyWorkingarea() {
     <>
       <div className={style.warea}>
         <div className={style.worktittle}>
-          <div className={style.box} id={style.todo}>Todo</div>
+          <div className={style.box} id={style.todo}> Todo</div>
           <div className={style.box} id={style.inporg}>Inprogress</div>
           <div className={style.box} id={style.review}>Review</div>
           <div className={style.box} id={style.done}>Done</div>
